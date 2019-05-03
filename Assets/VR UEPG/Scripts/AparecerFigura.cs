44fc3cdf39510438146c0a8fefcc5a10893c2e6d
Rullyan Gabriel dos Santos <rullyan.gs@hotmail.com>
2019-05-02 22:02:28 -0300

    Implementação do GoogleVR e controle gaze

---
+++ Assets/VR UEPG/Scripts/AparecerFigura.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AparecerFigura : MonoBehaviour{
+
+    public GameObject FiguraObject;
+    private bool Visibilidade;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Mostra ou esconde o objeto
+    public void ApareceOuDesapareceFigura()
+    {
+        if (!Visibilidade)
+        {
+            FiguraObject.SetActive(true);
+            Visibilidade = true;
+        }
+        else
+        {
+            FiguraObject.SetActive(false);
+            Visibilidade = false;
+        }
+    }
+
+    public void DesabilitarOutros()
+    {
+    FiguraObject.SetActive(false);
+    }
+}
---
+++ Assets/VR UEPG/Scripts/GazeInteracao.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class GazeInteracao : MonoBehaviour
+{
+
+    public Image imgCirculo;
+    public float gazeTime = 2;
+    private float timer;
+    bool gazedAt;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+            imgCirculo.fillAmount = timer / gazeTime;
+
+            if (timer > gazeTime)
+            {
+                // executa o manipulador de ponteiro
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0;
+            }
+        }
+
+    }
+
+    public void PointerEnter()
+    {
+        gazedAt = true;
+        Debug.Log("PointerEnter");
+    }
+
+    public void PointerExit()
+    {
+        timer = 0;
+        gazedAt = false;
+        imgCirculo.fillAmount = 0;
+        Debug.Log("PointerExit");
+        
+    }
+
+    public void PointerDown()
+    {
+        Debug.Log("PointerDown");
+    }
+}
