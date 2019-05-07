be63173167f437f2b046917557273424de9f0a18
Rullyan Gabriel dos Santos <rullyan.gs@hotmail.com>
2019-05-07 19:19:17 -0300

    Implementação do menu e fundo com tema espaço sideral

---
+++ Assets/VR UEPG/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject MenuObjeto;
+    private bool Visibilidade;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DesabilitaMenu()
+    {
+        MenuObjeto.SetActive(false);
+    }
+
+    public void ApareceBotoes()
+    {
+        MenuObjeto.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
