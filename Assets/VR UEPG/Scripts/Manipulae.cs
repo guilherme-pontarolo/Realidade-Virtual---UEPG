40c63843b5f01c96d62e1de4a1a8e2e5114d039b
Guilherme Pontarolo <guipontarolo@gmail.com>
2019-05-21 16:18:48 -0300

    Botões com som, alteração no quadro de objetos

--- Assets/VR UEPG/Scripts/Manipulae.cs
+++ Assets/VR UEPG/Scripts/Manipulae.cs
@@ -1,30 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Manipulae : MonoBehaviour
+
+public class Manipulae : Rotacionar
 {
-    private const int TAM = 1;
     private const int TEMPO = 300;
-    private const float ROT = 0.8F, COR = 0.03F;
     private float x, y, z;
-    public Color color = Color.black;
+    //public Color color = Color.black;
     private int cont = 0;
     bool liberaY = false, liberaZ = false;
-    Vector3 newSize;
+
     // Start is called before the first frame update
-    void iniciaCor()
-    {
-        color.r = 255f;
-        color.g = 0f;
-        color.b = 0f;
-        color.a = 0f;
-    }
     void Start()
-    {
-        iniciaCor();
-        gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, color.a);
+    {   
         transform.Rotate(Vector3.forward, 45);
+        gameObject.GetComponent<Renderer>().material.color = color;
     }
 
     // Update is called once per frame
@@ -33,14 +22,10 @@ public class Manipulae : MonoBehaviour
 
         if (cont > TEMPO)
         {
-/*             transform.Rotate(Vector3.up, ROT*7);
-            transform.Rotate(Vector3.left, ROT*4);
-            transform.Rotate(Vector3.forward, ROT*4);*/
-            transform.Rotate(ROT, ROT, ROT, Space.Self);
+             transform.Rotate(ROT, ROT, ROT, Space.Self);
             if (transform.localScale.x < TAM)
             {
                 x += 0.05f * Time.deltaTime;
-//                color.r += COR;
                 transform.localScale += new Vector3(x, 0, 0);
                 if (transform.localScale.x > TAM)
                     liberaY = true;
@@ -58,13 +43,9 @@ public class Manipulae : MonoBehaviour
         if(cont > TEMPO*5)
             if (liberaZ && transform.localScale.z < TAM)
             {
-//                color.b += COR;
-//                color.r -= COR;
-//                color.g -= COR / 2;
                 z += 0.05f * Time.deltaTime;
                 transform.localScale += new Vector3(0, 0, z);
             }
-        gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, color.a);
         cont++;
 
     }
---
+++ Assets/VR UEPG/Scripts/Rotacionar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacionar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected const double TAM = 2.5;
+    protected const float ROT = 0.8F, COR = 0.03F;
+    public double getTAM(){
+        return TAM;
+    }
+    public float getROT(){
+        return ROT;
+    }
+
+    protected float x, y, z;
+    public Color color = Color.red; 
+    void Start()
+    {
+        transform.Rotate(Vector3.forward, 45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(ROT, ROT, ROT, Space.Self);
+        transform.GetComponent<Renderer>().material.color = color;
+    }
+}
