72d93b62fd676cd61dfadde77b669dd1cc035e15
Guilherme Pontarolo <guipontarolo@gmail.com>
2019-05-23 16:47:00 -0300

    Sprites atualizadas, piramide arrumada
    É necessária a correção para aparecer a pirâmide

--- Assets/VR UEPG/Scripts/Rotacionar.cs
+++ Assets/VR UEPG/Scripts/Rotacionar.cs
@@ -7,24 +7,25 @@ public class Rotacionar : MonoBehaviour
     // Start is called before the first frame update
     protected const double TAM = 2.5;
     protected const float ROT = 0.8F, COR = 0.03F;
-    public double getTAM(){
-        return TAM;
-    }
-    public float getROT(){
-        return ROT;
-    }
-
+    public GameObject cubo;
+    GameObject obj;
     protected float x, y, z;
     public Color color = Color.red; 
     void Start()
     {
-        transform.Rotate(Vector3.forward, 45);
+
+        transform.GetComponent<Renderer>().material.color = color;
+        cubo.transform.Rotate(45f, 0f, 45f, Space.Self);
+       /* if(Equals(cubo)){
+            transform.Rotate(Vector3.forward, 45);
+            cubo.transform.Rotate(Vector3.forward, 45);
+        }
+         */
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(ROT, ROT, ROT, Space.Self);
-        transform.GetComponent<Renderer>().material.color = color;
+        transform.Rotate(0, ROT, 0, Space.World);
     }
 }
